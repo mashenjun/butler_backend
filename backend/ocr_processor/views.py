a964afb3b84ba98555300d262d5783eca2f3db10
Shenjun Ma <mashenjun0902@gmail.com>
2017-04-08 01:57:06 +0200

    .

--- backend/ocr_processor/views.py
+++ backend/ocr_processor/views.py
@@ -103,20 +103,21 @@ class receiptCreateView(generics.CreateAPIView):
             # use thread to send to OCR
             serializer.validated_data.get('picFile').seek(0)
             pool = ThreadPool(processes=1)
-            async_result = pool.apply_async(sendtoOCR, args = (serializer.validated_data, ))
+            async_result = pool.apply_async(sendtoS3, args = (serializer.validated_data, ))
+
             # response = requests.post(OCR_HOST, data=[('key', OCR_KEY), ('superMarket', superMarket)], files=files)
             # use thread to upload image to S3
-            s3_filename = os.path.join(superMarket, '{}_{}.{}'.format(uuid.uuid4().hex,'0','jpg'))
-            s3.upload_fileobj(img, BUCKET_NAME, s3_filename, ExtraArgs={'ACL':'public-read','ContentType':'image/jpeg'})
+            # s3_filename = os.path.join(superMarket, '{}_{}.{}'.format(uuid.uuid4().hex,'0','jpg'))
+            # s3.upload_fileobj(img, BUCKET_NAME, s3_filename, ExtraArgs={'ACL':'public-read','ContentType':'image/jpeg'})
             # bucket.put_object(ACL='public-read',Body=img)
             # bucket.upload_fileobj(img, s3_filename, ExtraArgs={'ACL':'public-read','ContentType':'image/jpeg'})
             bucket_location = s3.get_bucket_location(Bucket=BUCKET_NAME)
-            url = "https://s3-{0}.amazonaws.com/{1}/{2}".format(
-                bucket_location['LocationConstraint'],
-                BUCKET_NAME,
-                s3_filename)
-            processedResult = async_result.get()
-
+            # url = "https://s3-{0}.amazonaws.com/{1}/{2}".format(
+            #     bucket_location['LocationConstraint'],
+            #     BUCKET_NAME,
+            #     s3_filename)
+            processedResult = sendtoOCR(serializer.validated_data)
+            url = async_result.get()
             if(len(processedResult.content)==0):
                 processedResult_json = {}
             else:
